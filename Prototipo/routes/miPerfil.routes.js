f24c97362855cbe197bf1838957b5b70fbe1bdd8
M4uricio2002 <a01703337@tec.mx>
2024-03-08 13:24:30 -0600

    miPerfil.routes

--- Prototipo/routes/miPerfil.routes.js
+++ Prototipo/routes/miPerfil.routes.js
@@ -2,13 +2,8 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
-<<<<<<< HEAD
 router.get('/', (request, response, next) => {
     response.render('miPerfil');
 });
-=======
-const miPerfilController= require('../controllers/miPerfil.controller');
-router.get('/', miPerfilController.get_miPerfil);
->>>>>>> Mau/miPerfil
 
 module.exports = router;
