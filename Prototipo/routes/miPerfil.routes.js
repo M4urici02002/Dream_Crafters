8be0da270de418cc015f068edaaf2b6dffaaa20c
M4uricio2002 <a01703337@tec.mx>
2024-03-08 13:22:12 -0600

    Cambio en miPerfil routes

--- Prototipo/routes/miPerfil.routes.js
+++ Prototipo/routes/miPerfil.routes.js
@@ -1,9 +1,8 @@
 const express = require('express');
-
 const router = express.Router();
+const path = require('path');
 
-router.get('/miPerfil', (request, response, next) => {
-    response.render('miPerfil');
-});
+const miPerfilController= require('../controllers/miPerfil.controller');
+router.get('/', miPerfilController.get_miPerfil);
 
 module.exports = router;
