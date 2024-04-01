5602238286a417bc068ca384d3528dbdd9716ede
NicoH00d <a01705857@tec.mx>
2024-04-01 13:17:03 -0600

    desplegar resenas con base de datos

--- Prototipo/controllers/resena.controller.js
+++ Prototipo/controllers/resena.controller.js
@@ -1,7 +1,15 @@
 const Resena = require('../models/resena.model');
 
 exports.get_resena = (request, response, next) => {
-    response.render('resena',{
-        resena: Resena.fetchAll(),
+    Resena.fetchAll().then(([rows, fieldData]) => {
+        //console.log(fieldData);
+        response.render('resena', {
+            resena: rows,
+        });
+    })
+    .catch((error) => {
+        console.log(error)
     });
+
+
 };
--- Prototipo/models/resena.model.js
+++ Prototipo/models/resena.model.js
@@ -1,39 +1,44 @@
-const resena = [
-    {
-        producto:"Colchon Sigma",
-        nombre: "Valeria", 
-        fecha: "00/00/0000", 
-        titulo: "Muy buena calidad y muy comodo",
-        estrellas: 'xxxxx',
+const db = require('../util/database');
 
-    }
-];
+// const resena = [
+//     {
+//         producto:"Colchon Sigma",
+//         nombre: "Valeria", 
+//         fecha: "00/00/0000", 
+//         titulo: "Muy buena calidad y muy comodo",
+//         estrellas: 'xxxxx',
+
+//     }
+// ];
 
 module.exports = class Resena {
 
     //Constructor de la clase. Sirve para crear un nuevo objeto, y en él se definen las propiedades del modelo
-    constructor(mi_producto, mi_nombre, mi_fecha, mi_titulo, mi_estrellas) {
-        this.producto = mi_producto;
-        this.nombre = mi_nombre;
-        this.fecha = mi_fecha;
-        this.titulo = mi_titulo;
-        this.estrellas = mi_estrellas;
+    constructor(mi_IDEncuesta, mi_IDProducto, mi_IDCliente, mi_Titulo, mi_Rating, mi_Descripcion,mi_Enviada,mi_FechaContestacion) {
+        this.IDEncuesta = mi_IDEncuesta;
+        this.IDProducto = mi_IDProducto;
+        this.IDCliente = mi_IDCliente;
+        this.Titulo = mi_Titulo;
+        this.Rating = mi_Rating;
+        this.Descripcion = mi_Descripcion;
+        this.Enviada = mi_Enviada;
+        this.FechaContestacion = mi_FechaContestacion;
     }
 
     //Este método servirá para guardar de manera persistente el nuevo objeto. 
-    save() {
-        coches.push({
-            producto: this.producto,
-            nombre: this.nombre,
-            fecha: this.fecha,
-            titulo: this.titulo,
-            estrellas: this.estrellas,
-        });
-    }
+//    save() {
+//        coches.push({
+//            producto: this.producto,
+//            nombre: this.nombre,
+//            fecha: this.fecha,
+//            titulo: this.titulo,
+//            estrellas: this.estrellas,
+//        });
+//    }
 
     //Este método servirá para devolver los objetos del almacenamiento persistente.
     static fetchAll() {
-        return resena;
+        return db.execute('Select * from resena')
     }
 
 }
