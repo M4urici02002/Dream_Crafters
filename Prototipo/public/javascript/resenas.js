d22ba32603e6888ae652c8e7fd5e2e6a75719d60
PAOLA MARIA garrido <you@example.com>
2024-04-03 22:46:18 -0600

    arreglar boton reseñas

---
+++ Prototipo/public/javascript/resenas.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Obtener el valor seleccionado actualmente del menú desplegable
+    var categoriaSeleccionada = sessionStorage.getItem('categoriaSeleccionada');
+  
+    // Si hay una categoría seleccionada, establecerla como seleccionada en el menú desplegable
+    if (categoriaSeleccionada) {
+      document.querySelector('.categorias').value = categoriaSeleccionada;
+    }
+  
+    // Agregar un evento para guardar la selección cuando cambie el menú desplegable
+    document.querySelector('.categorias').addEventListener('change', function() {
+      sessionStorage.setItem('categoriaSeleccionada', this.value);
+    });
+});
