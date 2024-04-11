0a4b8776df3134790775b86d7488ca61776a998a
M4uricio2002 <a01703337@tec.mx>
2024-04-11 13:28:15 -0600

    Dropdown de productos y categoria

--- Prototipo/controllers/graficas.controller.js
+++ Prototipo/controllers/graficas.controller.js
@@ -1,16 +1,36 @@
-// controllers/tuControlador.js
-const calificacionModel = require('../models/graficas.model');
+const calificacionesModel = require('../models/graficas.model');
 
 exports.calificacionEstrellas = (req, res) => {
-    calificacionModel.obtenerCalificaciones()
-    .then(([result]) => {
+    Promise.all([
+        calificacionesModel.obtenerCategorias(),
+        calificacionesModel.obtenerCalificaciones(),
+    ])
+    .then(([categoriasResult, calificacionesResult]) => {
         res.render('calificacionEstrellas', {
-            datos: result,
-            permisos: req.session.permisos || [] 
+            categorias: categoriasResult[0],
+            datos: calificacionesResult[0],
+            permisos: req.session.permisos || []
         });
     })
-    .catch((error) => {
-        console.error('Error al obtener las calificaciones:', error);
-        res.send("Error al obtener los datos");
+    .catch(err => {
+        console.error('Error al obtener los datos:', err);
+        res.status(500).send("Error al obtener los datos");
     });
+};
+
+exports.productosPorCategoria = (req, res) => {
+    const categoriaSeleccionada = req.query.categoria; // Asumiendo que se pasa la categoría como un parámetro de consulta
+
+    if (!categoriaSeleccionada) {
+        return res.status(400).send("Categoría no especificada");
+    }
+
+    calificacionesModel.obtenerProductosPorCategoria(categoriaSeleccionada)
+        .then(([productosResult]) => {
+            res.json(productosResult); // Enviar los productos como respuesta JSON
+        })
+        .catch(err => {
+            console.error(`Error al obtener productos para la categoría ${categoriaSeleccionada}:`, err);
+            res.status(500).send("Error al obtener productos");
+        });
 };
--- Prototipo/routes/graficas.routes.js
+++ Prototipo/routes/graficas.routes.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const isAuth = require('../util/is-auth');
-const graficasController = require('../controllers/graficas.controller'); // Asegúrate de tener este archivo y función.
+const graficasController = require('../controllers/graficas.controller');
 
+// Ruta principal de 'graficas'
 router.get('/', isAuth, (request, response, next) => {
     response.render('graficas', {
          permisos: request.session.permisos || [],
     });
 });
 
-// Nueva ruta para calificación por estrellas
-
+// Ruta unificada para calificación por estrellas, categorías y productos
 router.get('/calificacionEstrellas', isAuth, graficasController.calificacionEstrellas);
+router.get("/productosPorCategoria", isAuth, graficasController.productosPorCategoria);
 module.exports = router;
