db86809a5dd54da9ea061e8d7bf2e7c7fb99cc4d
NicoH00d <a01705857@tec.mx>
2024-04-17 15:00:53 -0600

    cambios para el desplieuge

--- Prototipo/util/database.js
+++ Prototipo/util/database.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2');
 // Establecer el servidor
 
 const pool = mysql.createPool({
-    host: '146.190.155.149',
-    user: 'nicohood',
+    host: '127.0.0.1',
+    user: 'root',
     database: 'crafted_ratings', // Nombre del esquema en mySQL workbench
     password: '12345678'// Contraseña de tu conexion 
 });
