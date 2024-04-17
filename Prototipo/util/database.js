ef61ed71036196f499c8541bd2c0e1d9b52e4703
NicoH00d <a01705857@tec.mx>
2024-04-17 15:31:44 -0600

    revertir cambios

--- Prototipo/util/database.js
+++ Prototipo/util/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 // Establecer el servidor
 
 const pool = mysql.createPool({
-    host: '146.190.155.149',
+    host: '127.0.0.1',
     user: 'root',
     database: 'crafted_ratings', // Nombre del esquema en mySQL workbench
     password: '12345678'// Contraseña de tu conexion 
