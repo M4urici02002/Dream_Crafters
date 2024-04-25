f80770ea127e0a0a496e2a2a95c7b7d374d6ec91
Valeria Zuñiga Mendoza <A01705435@tec.mx>
2024-04-24 23:20:11 -0600

    Ajustes para convencion de código

--- Prototipo/controllers/gestionRoles.controller.js
+++ Prototipo/controllers/gestionRoles.controller.js
@@ -1,68 +1,80 @@
-const Rol = require('../models/rol.model');
-
-const db=require('../util/database');
+const Rol = require("../models/rol.model");
+const db = require("../util/database");
 
 exports.get_gestionRoles = async (request, response, next) => {
-    try {
-        const uroles = await Rol.fetchAll();
+  try {
+    const uroles = await Rol.fetchAll();
 
-        response.render('gestionRoles', {
-            rolRegistrado: uroles,
-            permisos: request.session.permisos || [],
-            csrfToken: request.csrfToken(),
-        });
-    } catch (error) {
-        console.log(error);
-        response.status(500).send("Error al obtener roles");
-    }
+    response.render("gestionRoles", {
+      rolRegistrado: uroles,
+      permisos: request.session.permisos || [],
+      csrfToken: request.csrfToken(),
+    });
+
+  } catch (error) {
+    console.log(error);
+    response.status(500).send("Error al obtener roles");
+  }
 };
-
 
 // Crear rol
 exports.get_crearRol = (request, response, next) => {
-    response.render('crearRol',{
-        permisos: request.session.permisos || [],
-        csrfToken: request.csrfToken(),
-    });
+  response.render("crearRol", {
+    permisos: request.session.permisos || [],
+    csrfToken: request.csrfToken(),
+  });
 };
 
-exports.post_crearRol = (request, response, next) => { //no
-    console.log(request.body);
-    const rol = new RolRegistrado(
-        request.body.nombreRol,
-    );
- 
-    rol.save()
-        .then(([rows, fieldData]) => {
-            response.redirect('/gestionRoles');
-        }).catch((error) => {
-            console.log(error);
-        });
+exports.post_crearRol = (request, response, next) => {
+  console.log(request.body);
+  const rol = new RolRegistrado(request.body.nombreRol);
+
+  rol
+    .save()
+    .then(([rows, fieldData]) => {
+      response.redirect("/gestionRoles");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 };
 
 // Eliminar rol
 exports.verificarAsignacion = async (request, response, next) => {
-    const { idrol } = request.body;
+  const { idrol } = request.body;
 
-    try {
-        // Verificar si el rol tiene asignaciones
-        const totalAsignaciones = await Rol.asignaciones(idrol);
+  try {
 
-        if (totalAsignaciones > 0) {
-            // Si tiene asignaciones, enviar un mensaje de error
-            console.log('El rol está asignado a uno o más usuarios y no puede ser eliminado.');
-            return response.status(400).json({ error: 'El rol está asignado a uno o más usuarios y no puede ser eliminado.' });
-        } else {
-            // Si no tiene asignaciones, proceder con la eliminación del rol
-            console.log("Rol a eliminar:", request.body.idrol);
-            await Rol.eliminar(idrol);
+    // Verificar si el rol tiene asignaciones
+    const totalAsignaciones = await Rol.asignaciones(idrol);
 
-            return response.status(200).json({ message: 'El rol fue eliminado correctamente.' });
-        }
+    if (totalAsignaciones > 0) {
 
-    } catch (error) {
-        console.error('Error al eliminar el rol:', error);
-        return response.status(500).json({ error: 'Error al eliminar el rol.' });
+      // Si tiene asignaciones, enviar un mensaje de error
+      console.log(
+        "El rol está asignado a uno o más usuarios y no puede ser eliminado."
+      );
+
+      return response
+        .status(400)
+        .json({
+          error:
+            "El rol está asignado a uno o más usuarios y no puede ser eliminado.",
+        });
+
+    } else {
+
+      // Si no tiene asignaciones, proceder con la eliminación del rol
+      console.log("Rol a eliminar:", request.body.idrol);
+      await Rol.eliminar(idrol);
+
+      return response
+        .status(200)
+        .json({ message: "El rol fue eliminado correctamente." });
     }
+    
+  } catch (error) {
+    console.error("Error al eliminar el rol:", error);
+    return response.status(500).json({ error: "Error al eliminar el rol." });
+  }
 };
-
--- Prototipo/models/rol.model.js
+++ Prototipo/models/rol.model.js
@@ -1,54 +1,60 @@
-const db = require('../util/database');
+const db = require("../util/database");
 
 module.exports = class Rol {
-    constructor(mi_nombreRol) {
-        this.nombreRol = mi_nombreRol;
-    } 
+  constructor(mi_nombreRol) {
+    this.nombreRol = mi_nombreRol;
+  }
 
-    async save() {
-        try {
-            await db.execute('INSERT INTO rol (nombre) VALUES (?)', [this.nombreRol]);
-        } catch(error) {
-            console.log(error);
-            throw new Error('Error al guardar el rol en la base de datos');
-        }
+  async save() {
+    try {
+      await db.execute("INSERT INTO rol (nombre) VALUES (?)", [this.nombreRol]);
+
+    } catch (error) {
+      console.log(error);
+      throw new Error("Error al guardar el rol en la base de datos");
     }
-    
-    static async fetchAll() {
-        try {
-            const [roles] = await db.query(`
+  }
+
+  static async fetchAll() {
+    try {
+      const [roles] = await db.query(`
                 SELECT * FROM rol;
             `);
-            return roles;
-        } catch (error) {
-            throw error;
-        }
-    }
+      return roles;
 
-    static eliminar(idrol) {
-        return db.execute('DELETE FROM rol WHERE idrol = ?', [idrol]);
+    } catch (error) {
+      throw error;
     }
+  }
 
-    static async asignaciones(idrol) {
-        try {
-            const result = await db.query('SELECT COUNT(*) AS total FROM asigna WHERE idrol = ?', [idrol]);
-            const totalAsignaciones = result[0][0].total;
-            console.log(totalAsignaciones);
-            return totalAsignaciones;
-        } catch (error) {
-            console.error('Error al verificar las asignaciones del rol:', error);
-            throw error;
-        }
-    }    
-    
+  static eliminar(idrol) {
+    return db.execute("DELETE FROM rol WHERE idrol = ?", [idrol]);
+  }
 
-    static async privilegioAll() {
-        try {
-            const [privilegios] = await db.query('SELECT * FROM privilegio');
-            console.log(privilegios);  // Agregar esta línea para ver la salida
-            return privilegios;
-        } catch (error) {
-            throw new Error('Error al obtener los privilegios de la base de datos');
-        }
+  static async asignaciones(idrol) {
+    try {
+      const result = await db.query(
+        "SELECT COUNT(*) AS total FROM asigna WHERE idrol = ?",
+        [idrol]
+      );
+      const totalAsignaciones = result[0][0].total;
+      console.log(totalAsignaciones);
+      return totalAsignaciones;
+
+    } catch (error) {
+      console.error("Error al verificar las asignaciones del rol:", error);
+      throw error;
     }
-}
+  }
+
+  static async privilegioAll() {
+    try {
+      const [privilegios] = await db.query("SELECT * FROM privilegio");
+      console.log(privilegios);
+      return privilegios;
+
+    } catch (error) {
+      throw new Error("Error al obtener los privilegios de la base de datos");
+    }
+  }
+};
