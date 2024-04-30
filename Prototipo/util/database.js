d71f41fcda1c7f34659d8c1f45a142a4c136b980
Ximena Cantera <A01277310@tec.mx>
2024-04-30 10:01:54 -0600

    Parámetros de nueva contraseña

--- Prototipo/util/database.js
+++ Prototipo/util/database.js
@@ -6,7 +6,7 @@ const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     database: 'crafted_ratings', // Nombre del esquema en mySQL workbench
-    password: '.Pm2paola'// Contraseña de tu conexion 
+    password: ''// Contraseña de tu conexion 
 });
 
 module.exports = pool.promise();
