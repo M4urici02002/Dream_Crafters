791567fac7331a03094792331c4b0f4e483b85ce
Valeria Zuñiga Mendoza <A01705435@tec.mx>
2024-04-30 10:15:54 -0600

    Desplegar tabla de compras

--- Prototipo/controllers/compras.controller.js
+++ Prototipo/controllers/compras.controller.js
@@ -2,18 +2,17 @@ const db = require("../util/database");
 const Compra = require('../models/compras.model');
 
 exports.get_catalogoCompras = async (request, response, next) => {
-    console.log("catalogo Compras");
    try {
-     //const uroles = await Rol.fetchAll();
+    const ucompras = await Compra.fetchAll();
 
-     response.render("compras", {
-       //rolRegistrado: uroles,
-       permisos: request.session.permisos || [],
-       csrfToken: request.csrfToken(),
-     });
+    response.render("compras", {
+        compras: ucompras,
+        permisos: request.session.permisos || [],
+        csrfToken: request.csrfToken(),
+    });
 
    } catch (error) {
      console.log(error);
-     response.status(500).send("Error al obtener catálogo de Encuestas");
+     response.status(500).send("Error al obtener catálogo de compras");
    }
 };
