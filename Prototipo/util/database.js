f2d614297918d9ffc8113a4daa0eb2b3a9aba7e5
Ximena Cantera <A01277310@tec.mx>
2024-05-01 16:53:18 -0600

    Iconos de exportar gráfica

--- Prototipo/util/database.js
+++ Prototipo/util/database.js
@@ -1,12 +1,14 @@
+require('dotenv').config();
+
 const mysql = require('mysql2');
 
 // Establecer el servidor
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'crafted_ratings', // Nombre del esquema en mySQL workbench
-    password: ''// Contraseña de tu conexion 
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD
 });
 
 module.exports = pool.promise();
