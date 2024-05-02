8232dc87af6cc10b4087852e868fdbee2a1b68cb
NicoH00d <a01705857@tec.mx>
2024-05-02 00:26:40 -0600

    arreglar consulta

--- Prototipo/models/compras.model.js
+++ Prototipo/models/compras.model.js
@@ -11,16 +11,17 @@ module.exports = class Compra {
   // Obtener todas las compras con información relacionada
   static async fetchAll(nombreMarca) {
       return db.execute(`
-              SELECT IDCompra, C.IDCliente, Cl.Nombre AS nombreCliente, Cl.Correo, C.IDProducto, P.Nombre AS nombreProducto, Fecha AS fechaCompra, E.IDEncuesta, E.Titulo AS tituloEncuesta, M.Nombre as NombreMarca
-              FROM compra C
-              INNER JOIN producto P ON C.IDProducto = P.IDProducto
-              INNER JOIN cliente Cl ON C.IDCliente = Cl.IDCliente
-              INNER JOIN encuesta E ON E.Categoria = P.Categoria
-              INNER JOIN marca M ON P.IDMarca = M.IDMarca
-              WHERE E.IDEncuesta = ( SELECT MAX(IDEncuesta)
-                        FROM encuesta 
-                        WHERE Categoria = P.Categoria and M.Nombre = ?)
-              ORDER BY C.IDCompra;
+      SELECT IDCompra, C.IDCliente, Cl.Nombre AS nombreCliente, Cl.Correo, C.IDProducto, P.Nombre AS nombreProducto, Fecha AS fechaCompra, E.IDEncuesta, E.Titulo AS tituloEncuesta, M.Nombre as NombreMarca
+      FROM compra C
+      INNER JOIN producto P ON C.IDProducto = P.IDProducto
+      INNER JOIN cliente Cl ON C.IDCliente = Cl.IDCliente
+      INNER JOIN encuesta E ON E.Categoria = P.Categoria
+      INNER JOIN marca M ON P.IDMarca = M.IDMarca
+      WHERE E.IDEncuesta = ( SELECT MAX(IDEncuesta)
+                FROM encuesta 
+                WHERE Categoria = P.Categoria and M.Nombre = ?)
+                AND DATE_ADD(C.Fecha, INTERVAL E.DiasParaEnvio DAY) = CURDATE()
+                ORDER BY c.IDCompra;
             `,[nombreMarca]);
     } 
   };
