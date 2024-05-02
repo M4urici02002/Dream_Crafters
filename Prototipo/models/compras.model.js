5b32f2c34d182719b7a451e2b1e107a82928292e
Valeria Zuñiga Mendoza <A01705435@tec.mx>
2024-05-02 00:02:10 -0600

    Cambios a compras

--- Prototipo/models/compras.model.js
+++ Prototipo/models/compras.model.js
@@ -12,17 +12,17 @@ module.exports = class Compra {
   static async fetchAll() {
     try {
       const [compras] = await db.query(`
-                SELECT IDCompra, C.IDCliente, Cl.Nombre AS nombreCliente, Cl.Correo, C.IDProducto, P.Nombre AS nombreProducto, Fecha AS fechaCompra, E.DiasParaEnvio, E.IDEncuesta, E.Titulo AS tituloEncuesta
-                FROM compra C
-                INNER JOIN Producto P ON C.IDProducto = P.IDProducto
-                INNER JOIN cliente Cl ON C.IDCliente = Cl.IDCliente
-                INNER JOIN encuesta E ON E.Categoria = P.Categoria
-                WHERE E.IDEncuesta IS NOT NULL
-                AND E.IDEncuesta = ( SELECT MAX(IDEncuesta)
-                    FROM encuesta 
-                    WHERE Categoria = P.Categoria)
-                AND DATE_ADD(C.Fecha, INTERVAL E.DiasParaEnvio DAY) = CURDATE() -- Comparación de fechas para determinar si se debe enviar hoy
-                ORDER BY C.IDCompra;
+              SELECT IDCompra, C.IDCliente, Cl.Nombre AS nombreCliente, Cl.Correo, C.IDProducto, P.Nombre AS nombreProducto, Fecha AS fechaCompra, E.DiasParaEnvio, E.IDEncuesta, E.Titulo AS tituloEncuesta
+              FROM compra C
+              INNER JOIN producto P ON C.IDProducto = P.IDProducto
+              INNER JOIN cliente Cl ON C.IDCliente = Cl.IDCliente
+              INNER JOIN encuesta E ON E.Categoria = P.Categoria
+              WHERE E.IDEncuesta IS NOT NULL
+              AND E.IDEncuesta = ( SELECT MAX(IDEncuesta)
+                FROM encuesta 
+                WHERE Categoria = P.Categoria)
+              AND DATE_ADD(C.Fecha, INTERVAL E.DiasParaEnvio DAY) = CURDATE()
+              ORDER BY C.IDCompra;
             `);
       return compras;
 
