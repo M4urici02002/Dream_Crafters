67c31be859d160bd032db4800402b9a1fcd67dac
NicoH00d <a01705857@tec.mx>
2024-05-01 22:44:41 -0600

    rbacGestionroles

--- Prototipo/routes/gestionRoles.routes.js
+++ Prototipo/routes/gestionRoles.routes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const isAuth = require('../util/is-auth');
+const canAdmin = require('../util/canAdmin');
 const gestionRolesController= require('../controllers/gestionRoles.controller');
 
-router.get('/gestionRoles', isAuth, gestionRolesController.get_gestionRoles);
-router.post('/gestionRoles/eliminar', isAuth, gestionRolesController.verificarAsignacion);
+router.get('/gestionRoles', isAuth, canAdmin, gestionRolesController.get_gestionRoles);
+router.post('/gestionRoles/eliminar', isAuth, canAdmin, gestionRolesController.verificarAsignacion);
 
 module.exports = router;
