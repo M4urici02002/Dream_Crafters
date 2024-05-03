e9f1d22d89cf261e7b1d44c7ae9ea98cc4438d22
PAOLA MARIA garrido <you@example.com>
2024-05-02 19:17:45 -0600

    envio de datos y diseño

--- Prototipo/routes/compras.routes.js
+++ Prototipo/routes/compras.routes.js
@@ -8,5 +8,6 @@ const canCrm = require('../util/canCrm');
 
 router.get('/catalogoCompras', isAuth, canCrm, comprasController.get_catalogoCompras);
 router.post('/catalogoCompras/enviarCorreo', isAuth, canCrm, comprasController.post_emailForm);
+router.post('/catalogoCompras/guardarDatosEncuesta', isAuth, canCrm, comprasController.post_guardarDatosEncuesta);
 
 module.exports = router;
